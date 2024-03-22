f6f9fd99956eec628178366c434748451a7f0b2e
seokcheon <my980519@gmail.com>
2024-03-22 08:20:15 +0000

    ..

---
+++ src/Scanners.java
@@ -0,0 +1,8 @@
+// 외부 입력
+import java.util.Scanner;
+
+public class Scanners {
+    public static void main(String[] args){     // static 과 main은 한 쌍으로 이루어진다.(규칙)
+        System.out.println("Hello Main !");
+    }
+}
