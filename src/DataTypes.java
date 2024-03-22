d06e299ef7a887ba81f144be26074b72b2a261e6
seokcheon <my980519@gmail.com>
2024-03-22 07:45:00 +0000

    .

---
+++ src/DataTypes.java
@@ -0,0 +1,15 @@
+public class DataTypes {
+    public static void main(String[] args){     // static 과 main은 한 쌍으로 이루어진다.(규칙)
+        // Numbers
+        int intFirst = 3;
+        System.out.println("Numbers int First : " + intFirst);
+        
+        boolean boolFirst = true ;
+        System.out.println("Boolean bool First : " + boolFirst);
+
+        String stringfirst = "Hello Datatypes!";
+        System.out.println("String string First : " + stringfirst);
+
+        return ;
+    }
+}
