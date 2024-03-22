c33a6ef9ee26842d60e93bbad92bc84e1bf22936
seokcheon <my980519@gmail.com>
2024-03-22 08:35:07 +0000

    .

--- src/Additions.java
+++ src/Additions.java
@@ -1,14 +1,14 @@
 // 외부 입력
 import java.util.Scanner;
 
-public class Scanners {
+public class Additions {
     public static void main(String[] args){     // static 과 main은 한 쌍으로 이루어진다.(규칙)
         Scanner scanner = new Scanner(System.in);
         System.out.print("intFirst : ");
         int intFirst = scanner.nextInt();
         System.out.print("intSecond : ");
         int intSecond = scanner.nextInt();
-
+        System.out.println(intFirst + intSecond);
         scanner.close();
     }
 }
