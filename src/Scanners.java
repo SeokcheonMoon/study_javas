0806cb386150a67658b510daf656b34893dfa52a
seokcheon <my980519@gmail.com>
2024-03-22 08:22:56 +0000

    .

--- src/Scanners.java
+++ src/Scanners.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 public class Scanners {
     public static void main(String[] args){     // static 과 main은 한 쌍으로 이루어진다.(규칙)
+        Scanner scanner
         System.out.println("Hello Main !");
     }
 }
